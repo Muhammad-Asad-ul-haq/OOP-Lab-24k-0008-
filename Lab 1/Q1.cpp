51f7a271f1f758028e968f504899a22d9b1f9e24
Muhammad-Asad-ul-haq <asadhaq01@gmail.com>
2025-01-27 06:49:50 -0800

    Q1.cpp

---
+++ Lab 1/Q1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Enter the Number: ";
+    cin >> num;
+    
+ 
+     if (num <= 1) {
+        cout << num << " is not a prime number "<< endl;
+    } else {
+    
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                cout << num << " is not a prime number" <<  endl;
+                return 0;
+            }
+        }
+        
+        cout << num << " is a prime number";
+    }
+    }
