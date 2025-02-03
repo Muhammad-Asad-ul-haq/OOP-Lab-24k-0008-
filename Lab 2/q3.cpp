8a39f6bbfca64d35168dd7204a97752579c6a391
Muhammad-Asad-ul-haq <asadhaq01@gmail.com>
2025-02-03 03:02:03 -0800

    q3.cpp

--- Lab 2/q3.cpp
+++ Lab 2/q3.cpp
@@ -33,22 +33,3 @@ int main() {
 
     return 0;
 }
-q3.cpp
-Displaying q3.cpp.
-Lab 02 (In-Lab Tasks)
-Sameer Faisal
-•
-Jan 30 (Edited Jan 31)
-Due Jan 31, 10:30 AM
-Submit Q2 & Q3
-Class comments
-Your work
-Turned in
-
-q2.cpp
-Text
-
-q3.cpp
-Text
-Work cannot be turned in after the due date
-Private comments
