9c8dcd5909b579e13a2c27c4a5e193993b8db654
Muhammad-Asad-ul-haq <asadhaq01@gmail.com>
2025-02-03 03:01:46 -0800

    q3.cpp

--- Lab 2/q3.cpp
+++ Lab 2/q3.cpp
@@ -1,42 +1,4 @@
-Skip to main content
-Google Classroom
-Classroom
-OOP (BAI-2A) - Lab
-Spring 2025
-  
-Home
-Calendar
-Enrolled
-To-do
-B
-BAI-2A (CCE )
-A
-AI-2A EW Theory S25
-A
-AI-2A EW Lab S25
-E
-Expository Writing Spring 2025
-M
-MVC
-BAI-2A (Spring 2025)
-D
-DLD (BAI-2A)
-(BAI-2A)
-D
-Digital Logic Design - Sp25
-BAI-2A
-B
-BAI (ICP )
-2A
-O
-OOP Spring 25
-BAI-2A
-O
-OOP (BAI-2A) - Lab
-Spring 2025
-Archived classes
-Settings
-Lab 02 (In-Lab Tasks)
+
 #include <iostream>
 using namespace std;
 
