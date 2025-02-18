5fa9c3d9535953800afbd6dc5b94f285aee2d977
Muhammad-Asad-ul-haq <asadhaq01@gmail.com>
2025-02-18 03:30:20 -0800

    q5.cpp

--- Lab 4/q5.cpp
+++ Lab 4/q5.cpp
@@ -15,8 +15,9 @@ public:
         name = "Unknown Player";
         position = "Bench Warmer";
         goals = 0;
-
-        cout << "Player name is: " << name << endl;
+        
+        cout<<"Default Player: "<< endl;
+        cout << "\nPlayer name is: " << name << endl;
         cout << "\nHis position is: " << position << endl;
         cout << "\nHis goals are: " << goals << endl;
     }
@@ -26,7 +27,8 @@ public:
         this->name = name;
     	this->position = position;
     	this->goals = goals;
-
+        
+        cout<<"Parameterized Player: "<<endl;
         cout << "\nPlayer name is: " << name << endl;
         cout << "\nHis position is: " << position << endl;
         cout << "\nHis goals are: " << goals << endl;
@@ -37,6 +39,7 @@ public:
     	this->position = "Mid Fielder";
     	this->goals = 10;
     	
+    	cout<<"Default Argument Player: "<<endl;
     	cout << "\nPlayer name is: " << name << endl;
         cout << "\nHis position is: " << position << endl;
         cout << "\nHis goals are: " << goals << endl;
@@ -47,20 +50,21 @@ public:
          this->name = p.name;
          this->position = p.position;
          this->goals = p.goals;
-
+        
+        cout<<"Cloned Player: "<<endl;
         cout << "\nPlayer name is: " << name << endl;
         cout << "\nHis position is: " << position << endl;
         cout << "\nHis goals are: " << goals  << endl;
     }
     
     void boostGoals(int goals) {
-        this->goals += goals;;
+        this->goals += goals;
         cout <<"\n"<< name << " has scored " << goals << " more goals! Total goals are now increased to: " << this->goals <<"\nHe is now having a Ballon Dor Worthy Season!!" <<endl;
     }
 };
 
 int main() {
-    cout << "-----------------------------" << endl;
+    
     FootballPlayer p1; 
      cout << "-----------------------------" << endl;
     FootballPlayer p2("Leroy Sane", "Right Wing",128 );
