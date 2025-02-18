a83acb7c9f2217165eb306d3ebbecd5c890df8fc
Muhammad-Asad-ul-haq <asadhaq01@gmail.com>
2025-02-18 03:25:51 -0800

    q5.cpp

---
+++ Lab 4/q5.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class FootballPlayer {
+private:
+    string name;
+    string position;
+    int goals;
+
+public:
+    
+    FootballPlayer() {
+        name = "Unknown Player";
+        position = "Bench Warmer";
+        goals = 0;
+
+        cout << "Player name is: " << name << endl;
+        cout << "\nHis position is: " << position << endl;
+        cout << "\nHis goals are: " << goals << endl;
+    }
+
+   
+    FootballPlayer(string name, string position, int goals) {
+        this->name = name;
+    	this->position = position;
+    	this->goals = goals;
+
+        cout << "\nPlayer name is: " << name << endl;
+        cout << "\nHis position is: " << position << endl;
+        cout << "\nHis goals are: " << goals << endl;
+    }
+    
+     FootballPlayer(string name){
+        this->name = name;
+    	this->position = "Mid Fielder";
+    	this->goals = 10;
+    	
+    	cout << "\nPlayer name is: " << name << endl;
+        cout << "\nHis position is: " << position << endl;
+        cout << "\nHis goals are: " << goals << endl;
+     }
+
+    
+     FootballPlayer(FootballPlayer& p) {
+         this->name = p.name;
+         this->position = p.position;
+         this->goals = p.goals;
+
+        cout << "\nPlayer name is: " << name << endl;
+        cout << "\nHis position is: " << position << endl;
+        cout << "\nHis goals are: " << goals  << endl;
+    }
+    
+    void boostGoals(int goals) {
+        this->goals += goals;;
+        cout <<"\n"<< name << " has scored " << goals << " more goals! Total goals are now increased to: " << this->goals <<"\nHe is now having a Ballon Dor Worthy Season!!" <<endl;
+    }
+};
+
+int main() {
+    cout << "-----------------------------" << endl;
+    FootballPlayer p1; 
+     cout << "-----------------------------" << endl;
+    FootballPlayer p2("Leroy Sane", "Right Wing",128 );
+     cout << "-----------------------------" << endl;
+    p2.boostGoals(25);
+     cout << "-----------------------------" << endl;
+    
+    FootballPlayer p3("Lewandowski");
+     cout << "-----------------------------" << endl;
+    
+    FootballPlayer p4(p2);  
+    cout << "-----------------------------" << endl;
+    
+    return 0;
+}
